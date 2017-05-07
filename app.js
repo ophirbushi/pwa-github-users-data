8dc0730a84a854789c54306bcb6a490ef99c795d
ophir <ophirbushi@gmail.com>
2017-05-07 21:18:37 +0300

    loader

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ if ('serviceWorker' in navigator) {
 }
 
 function onWindowLoad() {
-    navigator.serviceWorker.register('/sw.js')
-        .then(function (registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
+    // navigator.serviceWorker.register('/sw.js')
+    //     .then(function (registration) {
+    //         // Registration was successful
+    //         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    //     }, function (err) {
+    //         // registration failed :(
+    //         console.log('ServiceWorker registration failed: ', err);
+    //     });
 }
